131854278f74af80bf1e6d02227cc4ae1b77153e
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-03-04 18:18:46 +0300

    :zap: add binding adapter

---
+++ app/src/main/java/com/ezzy/devbyte/util/BindingAdapters.kt
@@ -0,0 +1,18 @@
+package com.ezzy.devbyte.util
+
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+//binding adapter used to hide the spinner once data is available
+@BindingAdapter("goneIfNotNull")
+fun goneIfNotNull(view: View, it: Any?){
+    view.visibility = if (it != null) View.GONE else View.VISIBLE
+}
+
+//binding adapter used to display images from URL using Glide
+@BindingAdapter("imageUrl")
+fun setImageUrl(imageView: ImageView, url: String) {
+    Glide.with(imageView.context).load(url).into(imageView)
+}
